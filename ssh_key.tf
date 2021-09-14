7765d207ef7e967b967568794795b8c801a9420f
Jun Matsushita <jun@wire.com>
2021-09-14 17:09:33 +0200

    With count

--- ssh_key.tf
+++ ssh_key.tf
@@ -4,11 +4,13 @@
 resource "tls_private_key" "root" {
   algorithm = "RSA"
   rsa_bits  = 4096
+  count      = var.ssh_key_create ? 1 : 0
 }
 
 # This persists the ssh key at the location specified by ssh_key_path.
 resource "local_file" "id_root" {
   filename          = var.ssh_key_path
-  sensitive_content = tls_private_key.root.private_key_pem
+  sensitive_content = tls_private_key.root[0].private_key_pem
   file_permission   = "0600"
+  count      = var.ssh_key_create ? 1 : 0
 }
