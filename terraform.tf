4beeac1df442da482212c426d482897a242fa7e1
Jun Matsushita <jun@wire.com>
2021-09-23 19:00:38 +0200

    Upgrade to rancher/rancher2 1.20.0

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,7 @@ terraform {
       # Not using yet as it seems to be supporting only a handful of configuration options https://github.com/rancher/terraform-provider-rancher2/pull/626/files#diff-ea87985827a5e33692d9699416d9144971b191e9688a542125e8f31b5133adbdR1034-R1050
       # source  = "git@github.com:rawmind0/terraform-provider-rancher2.git?ref=rke2"
       source  = "rancher/rancher2"
-      version = "1.13.0"
+      version = "1.20.0"
     }
     hcloud = {
       source  = "hetznercloud/hcloud"
