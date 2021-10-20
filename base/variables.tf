9b5af2a7f8a7cfd4ea7ca951c8c7184153c2abf2
Florian Klink <flokli@flokli.de>
2021-10-20 22:11:37 +0200

    base: remove unused variable "internalbalancerip"

--- base/variables.tf
+++ base/variables.tf
@@ -8,16 +8,13 @@ variable "network" {
   default     = "10.0.0.0/8"
   description = "network to use"
 }
+
 variable "subnetwork" {
   type        = string
   default     = "10.0.0.0/24"
   description = "subnetwork to use"
 }
-variable "internalbalancerip" {
-  type        = string
-  default     = "10.0.0.2"
-  description = "IP to use for control plane loadbalancer"
-}
+
 variable "lb_type" {
   type        = string
   default     = "lb11"
