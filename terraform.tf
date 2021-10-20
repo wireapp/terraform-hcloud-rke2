b02e84229162f45fb03c47c5df4a5453732ee9f2
Florian Klink <flokli@flokli.de>
2021-10-20 22:56:38 +0200

    simplify provider, required_providers block
    All we want is the hetznercloud/hcloud provider, in something like
    version 1.31.1. Remove the remaining rancher parts, and the lockfile,
    which might cause problems in nixpkgs-managed terraform providers.

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,8 @@
 terraform {
   required_providers {
-    rancher2 = {
-      # PR for rke2_config https://github.com/rancher/terraform-provider-rancher2/pull/626/files
-      # Not using yet as it seems to be supporting only a handful of configuration options https://github.com/rancher/terraform-provider-rancher2/pull/626/files#diff-ea87985827a5e33692d9699416d9144971b191e9688a542125e8f31b5133adbdR1034-R1050
-      # source  = "git@github.com:rawmind0/terraform-provider-rancher2.git?ref=rke2"
-      source  = "rancher/rancher2"
-      version = "1.20.0"
-    }
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.31.0"
+      version = "~> 1.31.1"
     }
   }
 }
