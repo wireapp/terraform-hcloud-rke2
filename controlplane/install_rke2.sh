89646f8e30c80f3aed989f21605e5114a16b6b3c
Florian Klink <flokli@flokli.de>
2021-10-21 09:58:41 +0200

    controlplane: fix private interface renaming, configure canal to use it

--- controlplane/install_rke2.sh
+++ controlplane/install_rke2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 internal_ip=$(curl -sfL http://169.254.169.254/hetzner/v1/metadata/private-networks | grep "ip:" | head -n 1| cut -d ":" -f2 | xargs)
-# internal_mac=$(curl -sfL http://169.254.169.254/hetzner/v1/metadata/private-networks | grep "mac_address:" | head -n 1 | awk '{print $2}')
-# echo -e "[Match]\nMACAddress=$internal_mac\n[Link]\nName=priv" > /etc/systemd/network/80-internal.link
+internal_mac=$(curl -sfL http://169.254.169.254/hetzner/v1/metadata/private-networks | grep "mac_address:" | head -n 1 | awk '{print $2}')
+
+# Add a .link file renaming the private interface to "priv"
+echo -e "[Match]\nMACAddress=$internal_mac\n[Link]\nName=priv" > /etc/systemd/network/80-private.link
+
+# Do the same change manually, so it doesn't need a reboot to apply
+internal_if=$(ip -br link | awk "\$3 ~ /$internal_mac/ {print \$1}")
+ip l set down dev $internal_if
+ip l set $internal_if name priv
+ip l set up dev priv
 
 # Add the internal ip as node-ip to rancher config
 jq --arg nodeip $internal_ip '. + {"node-ip": $nodeip}' < /etc/rancher/rke2/config.yaml > /etc/rancher/rke2/config.yaml.new
